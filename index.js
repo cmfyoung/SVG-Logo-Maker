c0e5215842b5240eb6ac1677e3c07fd40b4109e6
cmfyoung <cmfyoung7@gmail.com>
2023-12-20 17:44:44 -0500

    Test working

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require("./lib/shapes.js");
+const { Circle, Triangle, Square } = require("./lib/shapes.js");
 
 inquirer.prompt ([
 {
@@ -17,15 +17,35 @@ inquirer.prompt ([
 {
     type: 'input',
     message: 'Please provide the text color name or hexidecimal number: ',
-    name: 'text-color',
+    name: 'color',
 },    
 {
     type: 'input',
     message: 'Please provide the background color name or hexidecimal number: ',
-    name: 'background-color',
+    name: 'bgcolor',
 },    
 ])
 
 .then ((response) => {
     console.log(response)
+
+let svg;
+switch (response.shape) {
+    case 'Triangle':
+        svg = new Triangle(response);
+        break;
+    case 'Circle':
+        svg = new Circle(response);
+        break;
+    case 'Square':
+        svg = new Square(response);
+        break;
+}
+    
+    
+return fs.writeFile('logo.svg', svg.render(), (err) => {
+    if (err) throw err;
+    console.log('Logo generated! Saved as Logo.svg!')
+    });
 })
+
